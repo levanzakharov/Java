5d239013196cce38025e0f8a86e1fc1f36fe541e
LAB316 Student15 <LAB316_Student15@gau.local>
2019-09-23 18:29:04 +0400

    first commit

---
+++ week_1/src/gau/Main.java
@@ -0,0 +1,27 @@
+package gau;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    System.out.println("Hello world!");
+//	    int[] m = new int [4];
+//        int[] m1 = {1,2,4,5};
+//
+//        for (int i = 0; i<m1.length; i++){
+//            System.out.println(m1[i]);
+//        }
+//
+//        Random random = new Random();
+//        System.out.println(random.nextInt(10));
+
+        String s;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+
+        System.out.println(s);
+    }
+
+}
